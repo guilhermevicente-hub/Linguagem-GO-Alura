b909d21a681b1538b47a1a30fabbdfe586625807
guilhermevicente-hub <guilherme.vicszz@gmail.com>
2024-05-01 00:12:34 -0300

    capitulo 3 - fazendo requisiçoes para web

--- hello.go
+++ hello.go
@@ -1,29 +1,37 @@
 package main
 
-import "fmt"
-import "os"
+import (
+	"fmt"
+	"net/http"
+	"os"
+)
 
-func main() { 
+func main() {
 
 	exibeIntroducao()
-	exibeMenu()
-	comando := lerComando()
 
-	switch comando {
-	case 1:
-		fmt.Println("Monitorando...")
-	case 2:
-		fmt.Println("Exibindo Logs...")
-	case 0:
-		fmt.Println("Saindo do programa")
-		os.Exit(0)
-	default:
-		fmt.Println("Não conheço este comando")
-		os.Exit(-1)
+	for {
+
+		exibeMenu()
+		comando := lerComando()
+
+		switch comando {
+		case 1:
+			iniciarMonitoramento()
+		case 2:
+			fmt.Println("Exibindo Logs...")
+		case 0:
+			fmt.Println("Saindo do programa")
+			os.Exit(0)
+		default:
+			fmt.Println("Não conheço este comando")
+			os.Exit(-1)
+		}
 	}
+
 }
 
-func exibeIntroducao(){
+func exibeIntroducao() {
 	nome := "Guilherme"
 	versao := 1.1
 	fmt.Println("Olá, sr", nome)
@@ -34,25 +42,30 @@ func lerComando() int {
 	var comandoLido int
 	fmt.Scan(&comandoLido)
 	fmt.Println("O comando escolhido foi", comandoLido)
-	
+
 	return comandoLido
 }
 
-func exibeMenu(){
+func exibeMenu() {
 	fmt.Println("1 - Iniciar Monitoramento")
 	fmt.Println("2 - Exibir Logs")
 	fmt.Println("0 - Sair do Programa")
 }
 
+func iniciarMonitoramento() {
+	fmt.Println("Monitorando...")
+	site := "https://httpbin.org/status/404" // ou 200
+	resp, _ := http.Get(site)
+	// fmt.Println(resp)
 
-
-
-	// if comando == 1 {
-	// 	fmt.Println("Monitorando...")
-	// }else if comando == 2 {
-	// 	fmt.Println("Exibindo Logs...")
-	// }else if comando == 0 {
-	// 	fmt.Println("Saindo do programa")
-	// }else{
-	// 	fmt.Println("Não conheço este comando")
+	// if err != nil {
+	// 	fmt.Println("Ocorreu um erro:", err)
+	// 	return
 	// }
+
+	if resp.StatusCode == 200 {
+		fmt.Println("Site:", site, "foi carregado com sucesso!")
+	} else {
+		fmt.Println("Site:", site, "esta com problemas. Status Code:", resp.StatusCode)
+	}
+}
