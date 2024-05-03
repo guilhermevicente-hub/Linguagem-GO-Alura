4bdb8748745b8aa108eea11e616b6cfb9706da7b
guilhermevicente-hub <guilherme.vicszz@gmail.com>
2024-05-03 18:24:44 -0300

    Capitulo 7 - Escrevendo arquivos. Exibindo logs

--- hello.go
+++ hello.go
@@ -4,8 +4,10 @@ import (
 	"bufio"
 	"fmt"
 	"io"
+	"io/ioutil"
 	"net/http"
 	"os"
+	"strconv"
 	"strings"
 	"time"
 )
@@ -17,7 +19,6 @@ const delay = 5
 func main() {
 
 	exibeIntroducao()
-	lerSitesArquivo()
 
 	for {
 
@@ -29,6 +30,7 @@ func main() {
 			iniciarMonitoramento()
 		case 2:
 			fmt.Println("Exibindo Logs...")
+			imprimeLogs()
 		case 0:
 			fmt.Println("Saindo do programa")
 			os.Exit(0)
@@ -86,15 +88,15 @@ func testaSites(site string) {
 	resp, err := http.Get(site)
 
 	if err != nil {
-		fmt.Printf("Ocorreu um erro", err)
+		fmt.Println("Ocorreu um erro", err)
 	}
 
 	if resp.StatusCode == 200 {
 		fmt.Println("Site:", site, "foi carregado com sucesso!")
-		fmt.Println("")
+		regLog(site, true)
 	} else {
 		fmt.Println("Site:", site, "esta com problemas. Status Code:", resp.StatusCode)
-		fmt.Println("")
+		regLog(site, false)
 	}
 }
 
@@ -113,7 +115,7 @@ func lerSitesArquivo() []string {
 
 		linha, err := leitor.ReadString('\n')
 		linha = strings.TrimSpace(linha)
-		
+
 		sites = append(sites, linha)
 
 		if err == io.EOF {
@@ -124,4 +126,30 @@ func lerSitesArquivo() []string {
 	arquivo.Close()
 
 	return sites
+}
+
+func regLog(site string, status bool) {
+
+	arquivo, err := os.OpenFile("log.txt", os.O_RDWR|os.O_CREATE|os.O_APPEND, 0666)
+
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	arquivo.WriteString(time.Now().Format("02/01/2006 15:04:05") + " - " + site + "online: " + strconv.FormatBool(status) + "\n")
+
+	arquivo.Close()
+
+}
+
+func imprimeLogs() {
+
+	arquivo, err := ioutil.ReadFile("log.txt")
+
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	fmt.Println(string(arquivo))
+
 }
